770fbcac5e49a525015bfcc7cb6d1b28d7d28d33
Enrick3344 <enrico3815@hotmail.fr>
2017-02-04 16:54:07 -0500

    Adding New Mobs.

---
+++ src/slapper/entities/SlapperEnderDragon.php
@@ -0,0 +1,12 @@
+<?php
+namespace slapper\entities;
+
+use pocketmine\Player;
+use pocketmine\network\protocol\AddEntityPacket;
+use slapper\entities\SlapperEntity;
+
+class SlapperEnderDragon extends SlapperEntity {
+
+    public $entityId = 53;
+
+}
