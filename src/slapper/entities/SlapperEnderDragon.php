9abb26d9aa39fe6e19a6377835d6409abbff2b2f
Enrick3344 <enrico3815@hotmail.fr>
2017-02-05 20:02:43 -0500

    Update SlapperEnderDragon.php

--- src/slapper/entities/SlapperEnderDragon.php
+++ src/slapper/entities/SlapperEnderDragon.php
@@ -1,8 +1,6 @@
 <?php
 namespace slapper\entities;
 
-use pocketmine\Player;
-use pocketmine\network\protocol\AddEntityPacket;
 use slapper\entities\SlapperEntity;
 
 class SlapperEnderDragon extends SlapperEntity {
