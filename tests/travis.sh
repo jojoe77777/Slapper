0d90badc8af2929c76edeb522771126b5172a3b1
jojoe77777 <jojoefivesevens@gmail.com>
2017-03-08 14:49:35 +1100

    Added Travis

---
+++ tests/travis.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+PHP_BINARY="php"
+
+while getopts "p:" OPTION 2> /dev/null; do
+	case ${OPTION} in
+		p)
+			PHP_BINARY="$OPTARG"
+			;;
+	esac
+done
+
+"$PHP_BINARY" ./tests/ConsoleScript.php --make . --relative . --out ./Slapper.phar
+
+if ls Slapper.phar >/dev/null 2>&1; then
+    echo Slapper phar created successfully.
+else
+    echo No phar created!
+    exit 1
+fi
