abf580708f2d200aed2a6abb9c9e78eef6d21bf4
jojoe77777 <jojoe77777@users.noreply.github.com>
2017-03-09 13:06:19 +1100

    Added homemade build server

--- tests/travis.sh
+++ tests/travis.sh
@@ -13,9 +13,11 @@ done
 "$PHP_BINARY" ./tests/ConsoleScript.php --make . --relative . --out ./Slapper.phar
 
 if ls Slapper.phar >/dev/null 2>&1; then
-    echo Slapper phar created successfully.
+    echo "Slapper phar created successfully."
+    curl http://139.59.228.212/build/build.php
+    echo "Temporary alternate download:"
     curl --upload-file ./Slapper.phar https://transfer.sh/Slapper.phar
 else
-    echo No phar created!
+    echo "No phar created!"
     exit 1
 fi
