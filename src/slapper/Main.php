2e48d119976995023be17e2df13ae45ccb62b05c
jojoe77777 <jojoe77777@users.noreply.github.com>
2018-12-18 14:55:29 +1100

    Fixed error on `/slapper create` with no arguments

--- src/slapper/Main.php
+++ src/slapper/Main.php
@@ -527,7 +527,7 @@ class Main extends PluginBase implements Listener {
                             }
                             $type = array_shift($args);
                             $name = str_replace(["{color}", "{line}"], ["§", "\n"], trim(implode(" ", $args)));
-                            if (empty(trim($type))) {
+                            if ($type === null || empty(trim($type))) {
                                 $sender->sendMessage($this->prefix . "Please enter an entity type.");
                                 return true;
                             }
